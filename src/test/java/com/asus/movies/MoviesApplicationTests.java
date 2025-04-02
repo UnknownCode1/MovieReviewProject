baa5a70043d846d04a6e586d1cd74264da78a7d6
UnknownCode1 <balamshivaprasad2001@gmail.com>
2025-04-03 00:09:56 +0530

    Added Basic Spring layers to fetch allMovies from DB

---
+++ src/test/java/com/asus/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asus.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
